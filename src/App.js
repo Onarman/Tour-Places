c0c6a72a251414b93dd01b4afa859c5b5338e7e0
onarman <onarmannn@gmail.com>
2022-06-25 18:44:52 +0300

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Card from "./components/cards/Card";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import "./sass/App.scss";
+import data from "./helper/data";
 
 
 
 function App() {
   return (
     <div className="App">
-      <h1>asd</h1>
+      <Navbar />
+      <Header />
+      <Card />
     </div>
   );
 }
